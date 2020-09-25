eb56fd1f47608c28614325fe1c9a1dec933e09e3
even-gits <egej.vencelj@gmail.com>
2020-09-25 09:30:33 +0200

    *Update: Added logging option *Update: Improved help info

--- gmusic-dl.py
+++ gmusic-dl.py
@@ -19,17 +19,23 @@ def npath(path):
     return vfn(path, space='keep', initCap=False).decode('utf-8').rstrip('.')
 
 parser = ArgumentParser()
-parser.add_argument(dest='mail')
+parser.add_argument(dest='mail',
+    help='Your google mail for login.')
 parser.add_argument('-o', '--output', dest='output',
-                    default=os.path.join(os.getcwd(), 'Music'))
+                    default=os.path.join(os.getcwd(), 'Music'),
+    help='Destination to download files to. Default is ./Music/ (same folder)')
 #parser.add_argument('-p', '--password', dest='passwd')
-parser.add_argument('-i', '--device-id', dest='device_id', default=0)
+parser.add_argument('-i', '--device-id', dest='device_id', default=0,
+    help='Device ID to use - run without argument first to see available device IDs and then use one.')
+parser.add_argument('-l', '--log', dest='log', action='store_true',
+    help='Enable to have log printed to "log.txt".')
 
 args, unknown = parser.parse_known_args()
 
 login = args.mail
 targetDir = args.output
 
+#log_path = os.path.join(os.getcwd(), 'log.txt') if args.log else None
 
 device_id = args.device_id
 
@@ -57,6 +63,10 @@ if not logged_in:
 
 pbar = tqdm(api.get_all_songs())
 
+skipped = []
+downloaded = []
+failed = []
+
 for song in pbar:  # album['tracks']:
     dirName = os.path.join(
         npath(song['artist']),
@@ -70,19 +80,28 @@ for song in pbar:  # album['tracks']:
     if not os.path.exists(dirPath):
         os.makedirs(dirPath)
 
-    url = api.get_stream_url(song_id=song['id'], quality='hi')
     fileName = '{:02d}. {}'.format(
         song['trackNumber'],
         npath(song['title'])
     )
+
+    url = api.get_stream_url(song_id=song['id'], quality='hi')
+
     filePath = os.path.join(dirPath, fileName)
 
     if os.path.exists(filePath):
+        skipped.append(fileName)
         continue
 
     #print('downloading: ' + fileName)
     pbar.set_description(fileName)
-    urlretrieve(url, filePath)
+    try:
+        urlretrieve(url, filePath)
+        downloaded.append(fileName)
+    except Exception as e:
+        print(str(e))
+        failed.append(fileName)
+        continue
 
     audio = eyed3.load(filePath)
     if audio.tag is None:
@@ -93,5 +112,27 @@ for song in pbar:  # album['tracks']:
     audio.tag.title = song['title']
     audio.tag.track_num = song['trackNumber']
     audio.tag.save()
+
+if args.log:
+    try:
+        with open('log.txt', 'w') as f:
+            f.write(
+                (
+                    'D: {} F: {} S: {}\n'
+                    'Downloaded:\n'
+                    '\t{}\n'
+                    'Failed:\n'
+                    '\t{}\n'
+                    'Skipped:\n'
+                    '\t{}\n'
+                ).format(
+                    len(downloaded), len(failed), len(skipped),
+                    '\n\t'.join(downloaded),
+                    '\n\t'.join(failed),
+                    '\n\t'.join(skipped),
+                )
+            )
+    except Exception as e:
+        print(str(e))
 
 print('done!')
